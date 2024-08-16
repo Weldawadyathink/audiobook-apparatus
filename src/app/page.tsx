d5668bed5ecdbc9fabee49cfcaa6340d564091ae
Spenser Bushey <spenser@spenserbushey.com>
2024-08-16 12:59:56 -0700

    First draft rebuilt table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,104 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import { LoadingSpinner } from "@/components/LoadingSpinner";
 import { Download, ThumbsUp } from "lucide-react";
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 
 type LibraryItem = inferRouterOutputs<AppRouter>["audible"]["getLibrary"][0];
+
+const columns: ColumnDef<LibraryItem>[] = [
+  {
+    accessorKey: "asin",
+    header: "Asin",
+  },
+  {
+    accessorKey: "source",
+    header: "Source",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "title",
+    header: "Title",
+  },
+  {
+    accessorKey: "downloadPercentage",
+    header: "Download %",
+  },
+  {
+    accessorKey: "downloadSpeed",
+    header: "Speed",
+  },
+];
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
 
 function LibraryRow(params: {
   book: LibraryItem;
@@ -81,6 +177,12 @@ export default function Home() {
   function downloadAll() {
     void downloadAllQuery().then(() => void library.refetch());
   }
+
+  return (
+    <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+      {library.isSuccess && <DataTable data={library.data} columns={columns} />}
+    </div>
+  );
 
   return (
     <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
