6cd6ac6bde29183f6cb4888d8f92615b8e018d21
Spenser Bushey <spenser@spenserbushey.com>
2024-08-23 11:58:17 -0700

    Save new config file on server start

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,7 @@
 import { refreshAudibleJobSchedule } from "@/server/utils/cron-manager";
+import { getConfig, setConfig } from "@/config";
 
 export function register() {
   refreshAudibleJobSchedule();
+  setConfig(getConfig());
 }
